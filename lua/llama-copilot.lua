ad36a79301b491b578a45ffdf37d2566fb8c9ac5
Faywyn <63558304+Faywyn@users.noreply.github.com>
2024-04-05 15:45:00 +0200

    Update llama-copilot.lua

--- lua/llama-copilot.lua
+++ lua/llama-copilot.lua
@@ -7,7 +7,7 @@ local default_config = {
   host = "localhost",
   port = "11434",
   model = "codellama:7b-code",
-  max_completion_size = 15 -- use -1 for limitless
+  max_completion_size = -1 -- use -1 for limitless
 }
 M.config = {}
 for k, v in pairs(default_config) do M.config[k] = v end
@@ -32,20 +32,24 @@ local function on_update(chunk, job)
     res = body.response
   end
 
-  -- Check if window still open
-  if (not M.float_win or not vim.api.nvim_win_is_valid(M.float_win)) and M.running then
-    M.running = false
-    io.popen("kill " .. job.pid)
-    return
-  end
 
+  print(res)
+
+  -- Check if window still open
+  vim.schedule(function()
+    if (not M.float_win or not vim.api.nvim_win_is_valid(M.float_win)) and M.running then
+      M.running = false
+      io.popen("kill " .. job.pid)
+      return
+    end
+  end)
 
   M.res_txt = M.res_txt .. res
   M.res_txt = M.res_txt:gsub("```", "")
 
   -- Check max line
   local line_count = #vim.split(M.res_txt, "\n")
-  if (M.config.max_completion_size ~= -1 and M.config.max_completion_size <= line_count and M.running) then
+  if (M.config.max_completion_size > 0 and M.config.max_completion_size <= line_count and M.running) then
     M.running = false
     io.popen("kill " .. job.pid)
   end
@@ -56,8 +60,10 @@ local function on_update(chunk, job)
     M.res_txt = ""
   end
   if (M.res_txt ~= "") then
-    local table_res = Reduce_Array(vim.split(M.res_txt, "\n"), M.config.max_completion_size)
-    Append_to_buffer(M.res_buff, res)
+    vim.schedule(function()
+      Reduce_Array(vim.split(M.res_txt, "\n"), M.config.max_completion_size)
+      Append_to_buffer(M.res_buff, res)
+    end)
   end
 end
 
@@ -80,11 +86,9 @@ local function request(prompt)
       stream = true,
     }),
     stream = function(_, chunk, job)
-      vim.schedule(function()
-        if (M.running) then
-          on_update(chunk, job)
-        end
-      end)
+      if (M.running) then
+        on_update(chunk, job)
+      end
     end,
   })
 end
@@ -95,32 +99,3 @@ local function generate_code()
   M.line = vim.api.nvim_win_get_cursor(0)[1]
   local lines_arr = vim.api.nvim_buf_get_lines(0, 0, M.line, false)
 
-  local prompt = table.concat(lines_arr, "\n")
-
-  M.float_win = Create_Compl_Window(M.res_buff, M.config.max_completion_size)
-
-  if (Check_Plugins()) then
-    request(prompt)
-  else
-    vim.api.nvim_buf_set_lines(M.res_buff, 0, -1, true,
-      vim.split("[LlamaCopilot] You need to install the plenary.nvim plugin", "\n")
-    )
-  end
-end
-
--- Accept code
-local function accept_code()
-  -- Add M.res_buf content to current buffer
-
-  if (M.float_win) then
-    vim.api.nvim_win_close(M.float_win, true)
-  end
-
-  Append_to_buffe_line(0, M.res_txt, M.line)
-end
-
-vim.api.nvim_create_user_command("LlamaCopilotComplet", generate_code, {})
-vim.api.nvim_create_user_command("LlamaCopilotAccept", accept_code, {})
-Check_Plugins()
-
-return M

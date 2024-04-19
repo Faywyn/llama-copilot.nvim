3b48e2fad0a4b7992c3265274ad57b0b3275b3a3
Alban <alban.dllr@gmail.com>
2024-04-19 11:50:05 +0200

    Add safe call (pcall) + debug logs + make utils functions local

--- lua/llama-copilot.lua
+++ lua/llama-copilot.lua
@@ -1,5 +1,7 @@
-require("utils")
-
+local ok, utils = pcall(require, "utils")
+if not ok then
+  print "[LlamaCopilot] Loading error"
+end
 local M = {}
 
 -- Config --
@@ -7,7 +9,8 @@ local default_config = {
   host = "localhost",
   port = "11434",
   model = "codellama:7b-code",
-  max_completion_size = 15 -- use -1 for limitless
+  max_completion_size = 15, -- use -1 for limitless
+  debug = false,
 }
 M.config = {}
 for k, v in pairs(default_config) do M.config[k] = v end
@@ -17,13 +20,15 @@ M.res_buff = vim.api.nvim_create_buf(false, true)
 M.res_txt = ""
 M.line = 0
 
+utils.setup {
+  debug = M.config.debug
+}
+local log_debug = utils.log_debug
+local log = utils.log
 
 -- Add data response to M.res_buff
 local function on_update(chunk, job)
-  local _, body = pcall(function()
-    return vim.json.decode(chunk)
-  end)
-
+  local _, body = pcall(vim.json.decode, chunk)
   local res
 
   if (body == nil or body.response == nil) then
@@ -32,15 +37,22 @@ local function on_update(chunk, job)
     res = body.response
   end
 
-
   -- Check if window still open
-  vim.schedule(function()
-    if (not M.float_win or not vim.api.nvim_win_is_valid(M.float_win)) and M.running then
-      M.running = false
-      io.popen("kill " .. job.pid)
-      return
-    end
+  local status, err = pcall(function()
+    vim.schedule(function()
+      if (not M.float_win or not vim.api.nvim_win_is_valid(M.float_win)) and M.running then
+        log_debug("Killing job for closed window")
+        io.popen("kill " .. job.pid)
+        M.running = false
+        return
+      end
+    end)
   end)
+
+  if not status then
+    log_debug("Error while scheduling window cheking: " .. vim.inspect(err))
+  end
+
 
   M.res_txt = M.res_txt .. res
   M.res_txt = M.res_txt:gsub("```", "")
@@ -48,8 +60,9 @@ local function on_update(chunk, job)
   -- Check max line
   local line_count = #vim.split(M.res_txt, "\n")
   if (M.config.max_completion_size > 0 and M.config.max_completion_size <= line_count and M.running) then
-    M.running = false
+    log_debug("Killing job for max_completion_size")
     io.popen("kill " .. job.pid)
+    M.running = false
   end
 
 
@@ -58,17 +71,23 @@ local function on_update(chunk, job)
     M.res_txt = ""
   end
   if (M.res_txt ~= "") then
-    vim.schedule(function()
-      Reduce_Array(vim.split(M.res_txt, "\n"), M.config.max_completion_size)
-      Append_to_buffer(M.res_buff, res)
+    status, err = pcall(vim.schedule, function()
+      utils.reduce_array(vim.split(M.res_txt, "\n"), M.config.max_completion_size)
+      utils.append_to_buffer(M.res_buff, res)
     end)
+    if not status then
+      log_debug("Error while scheduling append_to_buffer: " .. vim.inspect(err))
+    end
   end
 end
 
 -- Reset buffer
 local function reset_buffer()
   M.res_txt = ""
-  vim.api.nvim_buf_set_lines(M.res_buff, 0, -1, true, {})
+  local status, err = pcall(vim.api.nvim_buf_set_lines, M.res_buff, 0, -1, true, {})
+  if not status then
+    log_debug("Error while reseting buffer: " .. vim.inspect(err))
+  end
 end
 
 -- Ask ollama
@@ -77,7 +96,7 @@ local function request(prompt)
   M.running = true
 
   local adress = "http://" .. M.config.host .. ":" .. M.config.port .. "/api/generate"
-  require("plenary.curl").post(adress, {
+  local status, err = pcall(require("plenary.curl").post, adress, {
     body = vim.json.encode({
       model = M.config.model,
       prompt = prompt,
@@ -89,40 +108,47 @@ local function request(prompt)
       end
     end,
   })
+
+  if not status then
+    log_debug("Error while calling plenary.post function: " .. vim.inspect(err))
+  end
 end
 
 -- Start code completion
 local function generate_code()
+  log_debug(":LlamaCopilotComplet command")
   -- Get above code
   M.line = vim.api.nvim_win_get_cursor(0)[1]
   local lines_arr = vim.api.nvim_buf_get_lines(0, 0, M.line, false)
 
   local prompt = table.concat(lines_arr, "\n")
 
-  M.float_win = Create_Compl_Window(M.res_buff, M.config.max_completion_size)
+  M.float_win = utils.create_compl_window(M.res_buff, M.config.max_completion_size)
 
-  if (Check_Plugins()) then
+  if (utils.check_plugins()) then
     request(prompt)
   else
     vim.api.nvim_buf_set_lines(M.res_buff, 0, -1, true,
-      vim.split("[LlamaCopilot] You need to install the plenary.nvim plugin", "\n")
+      vim.split("[LlamaCopilot] You need to install the required plugins", "\n")
     )
+    log("You need to install the required plugins")
   end
 end
 
 -- Accept code
 local function accept_code()
-  -- Add M.res_buf content to current buffer
+  log_debug(":LlamaCopilotAccept command")
 
+  -- Add M.res_buf content to current buffer
   if (M.float_win) then
     vim.api.nvim_win_close(M.float_win, true)
   end
 
-  Append_to_buffe_line(0, M.res_txt, M.line)
+  utils.append_to_buffe_line(0, M.res_txt, M.line)
 end
 
 vim.api.nvim_create_user_command("LlamaCopilotComplet", generate_code, {})
 vim.api.nvim_create_user_command("LlamaCopilotAccept", accept_code, {})
-Check_Plugins()
+utils.check_plugins()
 
 return M
--- lua/utils.lua
+++ lua/utils.lua
@@ -1,19 +1,38 @@
+local utils = {}
+utils.setup = function(conf) for k, v in pairs(conf) do utils[k] = v end end
+
+-- Log function
+local function log(str)
+  print("[LlamaCopilot] " .. str)
+end
+utils.log = log
+
+-- Log function for debug
+local function log_debug(str)
+  if utils.debug then
+    print("[LlamaCopilot Debug] " .. str)
+  end
+end
+utils.log_debug = log_debug
+
 -- Check require plugins
 local require_plugins = { "plenary" }
-function Check_Plugins()
+local function check_plugins()
+  log_debug("Checking required plugins ...")
   local valid = true
   for _, v in pairs(require_plugins) do
     local ok, _ = pcall(require, v)
     if not ok then
-      print("[LlamaCopilot] You need to install " .. v .. " plugin")
+      log("You need to install " .. v .. " plugin")
       valid = false
     end
   end
   return valid
 end
+utils.check_plugins = check_plugins
 
 -- Create floating window with buffer
-function Create_Compl_Window(buffer, height_)
+local function create_compl_window(buffer, height_)
   -- Get new window size
   local posX = -vim.api.nvim_win_get_cursor(0)[2]
   local width = vim.o.columns
@@ -30,14 +49,19 @@ function Create_Compl_Window(buffer, height_)
     border = "single",
   }
 
+  log_debug("Creating window opt: " .. vim.inspect(opts))
   local f_type = vim.bo.filetype
-  vim.api.nvim_buf_set_option(buffer, 'filetype', f_type)
+  local status, err = pcall(vim.api.nvim_buf_set_option, buffer, 'filetype', f_type)
+  if not status then
+    utils.log_debug("Error while creating window " .. vim.inspect(err))
+  end
 
   return vim.api.nvim_open_win(buffer, true, opts)
 end
+utils.create_compl_window = create_compl_window
 
 -- Reduce array size
-function Reduce_Array(arr, length)
+local function reduce_array(arr, length)
   if (length == -1 or #arr <= length) then
     return arr
   else
@@ -47,19 +71,30 @@ function Reduce_Array(arr, length)
     return table
   end
 end
+utils.reduce_array = reduce_array
 
 -- Append to buffer
-function Append_to_buffer(buf, str)
+local function append_to_buffer(buf, str)
   local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
   local last_line = lines[#lines]
   local new_lines = vim.split(str, "\n")
 
-  vim.api.nvim_buf_set_text(buf, #lines - 1, #last_line, #lines - 1, #last_line, new_lines)
+  local status, err = pcall(vim.api.nvim_buf_set_text, buf, #lines - 1, #last_line, #lines - 1, #last_line, new_lines)
+  if not status then
+    log_debug("Error while appending to buffer: " .. vim.inspect(err))
+  end
 end
+utils.append_to_buffer = append_to_buffer
 
 -- Append to buffer line
-function Append_to_buffe_line(buf, str, line)
+local function append_to_buffe_line(buf, str, line)
   local new_lines = vim.split(str, "\n")
 
-  vim.api.nvim_buf_set_text(buf, line, 0, line, 0, new_lines)
+  local status, err = pcall(vim.api.nvim_buf_set_text, buf, line, 0, line, 0, new_lines)
+  if not status then
+    log_debug("Error while appending to buffer at line (" .. line .. "): " .. vim.inspect(err))
+  end
 end
+utils.append_to_buffe_line = append_to_buffe_line
+
+return utils
